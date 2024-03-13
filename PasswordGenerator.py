abddf8203402a39b10a367fe8444c9013a8142fd
Gibbs <jgcomputers132@gmail.com>
2024-03-13 13:01:04 -0400

    User Password input hidden - Run in Terminal

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -14,9 +14,32 @@ def hash_password_with_salt(password, salt):
     hashed_password = hashlib.sha256(salted_password).hexdigest()
     return hashed_password
 
+def getpass(prompt="Password: "):
+    if os.name == 'nt':
+        import msvcrt
+    else:
+        import tty
+        import sys
+
+    print(prompt, end='', flush=True)
+    password = b''
+    while True:
+        char = msvcrt.getch() if os.name == 'nt' else sys.stdin.buffer.read(1)
+        if char in {b'\n', b'\r'}:
+            print()
+            break
+        elif char in {b'\x7f', b'\b'}:  # Backspace/Delete
+            if password:
+                password = password[:-1]
+                sys.stdout.write('\b \b')  # Erase the last character
+        else:
+            password += char
+            sys.stdout.write('*')  # Print '*' instead of the character
+    return password.decode()
+
 def main():
-    # Prompt user to input password
-    password = input("Enter your password: ")
+    # Prompt user to input password without showing characters
+    password = getpass("Enter your password: ")
 
     # Generate a random salt
     salt = os.urandom(16)
